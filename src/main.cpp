3bf484fd59f2693e0aa12c21bbca43e76e900178
vavrines <tianbaixiao@gmail.com>
2021-11-24 22:02:36 +0100

    Clean main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,32 @@
-/* Finite Volume Method for Linear Boltzmann Equation */
+/***
+ * Finite Volume Method for Linear Boltzmann Equation
+ * (c) 2021 Tianbai Xiao
+ *  
+***/
 
-#include <Python.h>
-#define PY_ARRAY_UNIQUE_SYMBOL KITRT_ARRAY_API
 #include <mpi.h>
 #include <string>
-
 #include "common/config.hpp"
 #include "common/io.hpp"
 #include "solvers/solverbase.hpp"
 
 int main( int argc, char** argv ) {
+    // preprocess
     MPI_Init( &argc, &argv );
-    wchar_t* program = Py_DecodeLocale( argv[0], NULL );
-    Py_SetProgramName( program );
-
     std::string filename = ParseArguments( argc, argv );
-
-    // CD  Load Settings from File
     Config* config = new Config( filename );
 
-    // Print input file and run info to file
+    // log
     PrintLogHeader( filename );
 
-    // Build solver
+    // solver
     SolverBase* solver = SolverBase::Create( config );
-
-    // Run solver and export
     solver->Solve();
     solver->PrintVolumeOutput();
+
+    // postprocess
     delete solver;
-
     delete config;
-
     MPI_Finalize();
 
     return EXIT_SUCCESS;
