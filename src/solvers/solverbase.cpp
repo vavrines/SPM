e8578e6fe9b98c5e489bd03ed6e1a01ba44b3af1
vavrines <tianbaixiao@gmail.com>
2021-11-25 16:16:55 +0100

    Remove csd part

--- src/solvers/solverbase.cpp
+++ src/solvers/solverbase.cpp
@@ -6,11 +6,6 @@
 #include "fluxes/numericalflux.hpp"
 #include "problems/problembase.hpp"
 #include "quadratures/quadraturebase.hpp"
-#include "solvers/csdpnsolver.hpp"
-#include "solvers/csdsnsolver.hpp"
-#include "solvers/csdsolvertrafofp.hpp"
-#include "solvers/csdsolvertrafofp2d.hpp"
-#include "solvers/csdsolvertrafofpsh2d.hpp"
 #include "solvers/mnsolver.hpp"
 #include "solvers/pnsolver.hpp"
 #include "solvers/snsolver.hpp"
@@ -100,14 +95,9 @@ SolverBase::~SolverBase() {
 SolverBase* SolverBase::Create( Config* settings ) {
     switch( settings->GetSolverName() ) {
         case SN_SOLVER: return new SNSolver( settings );
-
         case PN_SOLVER: return new PNSolver( settings );
         case MN_SOLVER: return new MNSolver( settings );
-        case CSD_SN_SOLVER: return new CSDSNSolver( settings );
-        case CSD_SN_FOKKERPLANCK_TRAFO_SOLVER: return new CSDSolverTrafoFP( settings );
-        case CSD_SN_FOKKERPLANCK_TRAFO_SOLVER_2D: return new CSDSolverTrafoFP2D( settings );
-        case CSD_SN_FOKKERPLANCK_TRAFO_SH_SOLVER_2D: return new CSDSolverTrafoFPSH2D( settings );
-        case CSD_PN_SOLVER: return new CSDPNSolver( settings );
+        
         default: ErrorMessages::Error( "Creator for the chosen solver does not yet exist. This is is the fault of the coder!", CURRENT_FUNCTION );
     }
     ErrorMessages::Error( "Creator for the chosen solver does not yet exist. This is is the fault of the coder!", CURRENT_FUNCTION );
